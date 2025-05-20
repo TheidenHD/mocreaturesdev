f100832637a1ddc1d40adeac7d93d1d829961ea0
multision <masond3v@protonmail.com>
2025-05-19 22:21:14 -0400

    Code cleanup

--- src/main/java/drzhark/mocreatures/MoCreatures.java
+++ src/main/java/drzhark/mocreatures/MoCreatures.java
@@ -3,57 +3,40 @@
  */
 package drzhark.mocreatures;
 
-import com.google.common.base.Supplier;
 import com.mojang.authlib.GameProfile;
 import drzhark.mocreatures.client.MoCKeyHandler;
 import drzhark.mocreatures.client.renderer.fx.MoCParticles;
 import drzhark.mocreatures.compat.CompatHandler;
-import drzhark.mocreatures.compat.datafixes.BlockIDFixer;
-import drzhark.mocreatures.compat.datafixes.EntityIDFixer;
-import drzhark.mocreatures.compat.datafixes.ItemIDFixer;
-import drzhark.mocreatures.compat.tinkers.TinkersConstructIntegration;
-import drzhark.mocreatures.dimension.MoCWorldProviderWyvernSkylands;
 import drzhark.mocreatures.entity.MoCEntityData;
 import drzhark.mocreatures.entity.tameable.MoCPetMapData;
 import drzhark.mocreatures.event.MoCEventHooks;
 import drzhark.mocreatures.event.MoCEventHooksClient;
 import drzhark.mocreatures.event.MoCEventHooksTerrain;
+import drzhark.mocreatures.init.MoCBiomesInit;
 import drzhark.mocreatures.init.MoCCreativeTabs;
 import drzhark.mocreatures.init.MoCEntities;
-import drzhark.mocreatures.init.MoCRecipes;
+import drzhark.mocreatures.init.MoCSoundEvents;
 import drzhark.mocreatures.network.MoCMessageHandler;
-import drzhark.mocreatures.network.command.CommandMoCPets;
-import drzhark.mocreatures.network.command.CommandMoCSpawn;
-import drzhark.mocreatures.network.command.CommandMoCTP;
-import drzhark.mocreatures.network.command.CommandMoCreatures;
 import drzhark.mocreatures.proxy.MoCProxy;
 import drzhark.mocreatures.proxy.MoCProxyClient;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.util.datafix.FixTypes;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.RegistryKey;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.DimensionType;
-import net.minecraft.world.biome.Biome;
-import net.minecraftforge.common.DimensionManager;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.util.ModFixs;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.*;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Map;
 import java.util.UUID;
 
 @Mod(MoCConstants.MOD_ID)
@@ -88,9 +71,6 @@ public class MoCreatures {
         if (true) {
             MinecraftForge.EVENT_BUS.register(new MoCEventHooksClient());
             MinecraftForge.EVENT_BUS.register(new MoCKeyHandler());
-            if (Loader.isModLoaded("tconstruct")) {
-                MinecraftForge.EVENT_BUS.register(new TinkersConstructIntegration());
-            }
         }
         //MoCEntities.registerEntities();
         CompatHandler.preInit();
@@ -100,54 +80,29 @@ public class MoCreatures {
         proxy.configInit();
         proxy.registerRenderers();
         proxy.registerRenderInformation();
-        WYVERN_SKYLANDS = DimensionType.register("Wyvern Skylands", "_wyvern_skylands", wyvernSkylandsDimensionID, MoCWorldProviderWyvernSkylands.class, false);
-        DimensionManager.registerDimension(wyvernSkylandsDimensionID, WYVERN_SKYLANDS);
-        MoCEventHooksTerrain.addBiomeTypes();
-        MoCEntities.registerSpawns();
-        MoCEventHooksTerrain.buildWorldGenSpawnLists();
-        MoCRecipes.registerOreDictionaries();
+
+        MoCEventHooksTerrain.addBiomeTypes(); //TODO FINISHED
+        //MoCBiomes.registerBiomes();
+        MoCBiomesInit.setupSpawnRules();
+        MoCEntities.registerEntities();
+
         CompatHandler.init();
-        ModFixs modFixer = FMLCommonHandler.instance().getDataFixer().init(MoCConstants.MOD_ID, MoCConstants.DATAFIXER_VERSION);
-        modFixer.registerFix(FixTypes.BLOCK_ENTITY, new BlockIDFixer());
-        modFixer.registerFix(FixTypes.ENTITY, new EntityIDFixer());
-        modFixer.registerFix(FixTypes.ITEM_INSTANCE, new ItemIDFixer());
+        registerDeferredRegistries(eventBus);
+
+        //ModFixs modFixer = FMLCommonHandler.instance().getDataFixer().init(MoCConstants.MOD_ID, MoCConstants.DATAFIXER_VERSION); //TODO TheidenHD
+        //modFixer.registerFix(FixTypes.BLOCK_ENTITY, new BlockIDFixer());
+        //modFixer.registerFix(FixTypes.ENTITY, new EntityIDFixer());
     }
 
     public static boolean isServer(World world) {
         return !world.isRemote();
     }
 
-    @EventHandler
-    public void loadComplete(FMLLoadCompleteEvent event) {
-        if (MoCreatures.proxy.debug) {
-            for (Biome biome : ForgeRegistries.BIOMES.getValuesCollection()) {
-                for (Biome.SpawnListEntry entry : biome.getSpawnableList(EnumCreatureType.CREATURE)) {
-                    LOGGER.info("Creature is spawnable in biome " + biome.biomeName + ": " + entry.entityClass);
-                }
-                for (Biome.SpawnListEntry entry : biome.getSpawnableList(EnumCreatureType.WATER_CREATURE)) {
-                    LOGGER.info("Water Creature is spawnable in biome " + biome.biomeName + ": " + entry.entityClass);
-                }
-                for (Biome.SpawnListEntry entry : biome.getSpawnableList(EnumCreatureType.MONSTER)) {
-                    LOGGER.info("Monster is spawnable in biome " + biome.biomeName + ": " + entry.entityClass);
-                }
-                for (Biome.SpawnListEntry entry : biome.getSpawnableList(EnumCreatureType.AMBIENT)) {
-                    LOGGER.info("Ambient is spawnable in biome " + biome.biomeName + ": " + entry.entityClass);
-                }
-            }
-        }
+    public static void registerDeferredRegistries(IEventBus modBus) {
+        MoCSoundEvents.SOUND_DEFERRED.register(modBus);
+        MoCParticles.PARTICLES.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
-    @EventHandler
-    public void serverStarting(FMLServerStartingEvent event) {
-        event.registerServerCommand(new CommandMoCreatures());
-        event.registerServerCommand(new CommandMoCTP());
-        event.registerServerCommand(new CommandMoCPets());
-        if (isServer()) {
-            if (FMLCommonHandler.instance().getMinecraftServerInstance().isDedicatedServer()) {
-                event.registerServerCommand(new CommandMoCSpawn());
-            }
-        }
-    }
 
 //    @EventHandler
 //    public void postInit(FMLPostInitializationEvent event) {
